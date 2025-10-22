f78df9d591c21d6bbe814022fe9c349ff57408c2
Oddis Nur Alifathur Razaaq <oddisnur@gmail.com>
2025-10-22 13:55:54 +0700

    Dashboard superadmin update

--- app/src/pages/superAdmin/Dashboard.jsx
+++ app/src/pages/superAdmin/Dashboard.jsx
@@ -865,7 +865,6 @@ const Dashboard = () => {
       minHeight: '100vh',
       backgroundColor: '#e7f3f5ff',
       padding: '24px',
-      paddingTop: '105px'
     }}>
       <div style={{
         maxWidth: '80rem',
